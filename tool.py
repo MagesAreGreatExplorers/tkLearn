d461be0dae221ae1fa5b7bcb3bb6a66980df56fa
Wrench <92103623+DengEditsNewGraphs@users.noreply.github.com>
2022-09-28 10:55:14 +0800

    Delete tool.py

--- tool.py
+++
@@ -1,7 +0,0 @@
-import base64
-read_f = open('forLight.png', 'rb')
-b64string = base64.b64encode(read_f.read()).decode('utf-8')
-read_f.close()
-write_f = open('image.py', 'a')
-write_f.write(f'light = \'{b64string}\'\n')
-write_f.close()
